0504f169d117bf26fcf27fd2e898982a3f06cf34
unknown <mohammad doar@DESKTOP-TVETFEV.(none)>
2023-06-20 13:34:38 +0300

    add card active and sideBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import CardAbout from "./components/CardAbout";
 import NavBar from "./components/NavBar";
 import "./css/index.css";
+import CardActive from "./components/CardActive";
+import CardMightKnow from "./components/CardMightKnow";
+import Sidebar from "./components/SideBar";
 function App() {
   return (
     <div className="bodyApp">
       <NavBar />
-      <CardAbout />
+      <Sidebar />
     </div>
   );
 }
