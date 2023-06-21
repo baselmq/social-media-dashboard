270e0b740fdedc383c5b91bb46403dc0d8b28fb8
Basel Qarqa <basel.m.qarqa@gmail.com>
2023-06-21 10:12:43 +0300

    edit file data

--- src/data/data.js
+++ src/data/data.js
@@ -1,22 +1,22 @@
 const userData = [
   {
-    id:0,
+    id: 0,
     firstName: "mohammad",
     lastName: "Mahmoud",
     image:
       "https://img.freepik.com/premium-photo/portrait-smiling-young-man-looking-camera_33839-1731.jpg",
-      imageCover:'',
+    imageCover: "",
     gender: "Male",
     location: "Amman - Jordan",
     birthDate: "June 26, 1980",
     email: "charles5182@ummoh.com",
     phone: "33757005467",
     statusLogin: "true",
-    friends: [{}],
-    friendsRequest: [{}],
+    friends: [{ fullName: "", email: "" }],
+    friendsRequest: [{ fullName: "", email: "" }],
     post: [
       {
-        id:0,
+        id: 0,
         dataPost: { image: "", content: "", datePost: "" },
         likes: [{ fullName: "", email: "", like: true }],
         comments: [
