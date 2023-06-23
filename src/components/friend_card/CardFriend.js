7f3ad620f1f4c2381625ca3dc1cd25130cc9600f
nadeen adel <nadeenadel.alirr@gmail.com>
2023-06-23 02:27:19 -0700

    add update

--- src/components/friend_card/CardFriend.js
+++ src/components/friend_card/CardFriend.js
@@ -9,8 +9,8 @@ const CardFriend = () => {
       <div className="Header__card">Friends</div>
     </div>
     {/* from here card  */}
-    <div className="row d-flex justify-content-center gap-2 mt-4">
-    <div className="col-md-5 border rounded p-3 pt-1 pb-1">
+    <div className="row d-flex justify-content-center gap-4 mt-3 ">
+    <div className="col-md-5 border rounded p-3 pt-2 pb-2 col_card">
         <div className="row d-flex align-items-center">
 
           <div className="col-2 p-0 " style={{ maxWidth: "100%", height: "55px" }}>
@@ -23,7 +23,33 @@ const CardFriend = () => {
           </div>
         </div>
       </div>
-      <div className="col-md-5 border rounded p-3 pt-1 pb-1">
+      <div className="col-md-5 border rounded p-3 pt-2 pb-2 col_card">
+        <div className="row d-flex align-items-center">
+
+          <div className="col-2 p-0 " style={{ maxWidth: "100%", height: "55px" }}>
+            <img src={userData[0].image} style={{ width: "100%", height: "55px", objectFit: "cover" }} className="rounded"></img>
+          </div>
+
+          <div className="col-5 d-flex justify-content-center flex-column">
+            <p className="d-flex align-items-center m-0 content_card">name</p>
+            <p className="d-flex align-items-center m-0 content_card2">10 mutual friends</p>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-5 border rounded p-3 pt-2 pb-2 col_card">
+        <div className="row d-flex align-items-center">
+
+          <div className="col-2 p-0 " style={{ maxWidth: "100%", height: "55px" }}>
+            <img src={userData[0].image} style={{ width: "100%", height: "55px", objectFit: "cover" }} className="rounded"></img>
+          </div>
+
+          <div className="col-5 d-flex justify-content-center flex-column">
+            <p className="d-flex align-items-center m-0 content_card">name</p>
+            <p className="d-flex align-items-center m-0 content_card2">10 mutual friends</p>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-5 border rounded p-3 pt-2 pb-2 col_card">
         <div className="row d-flex align-items-center">
 
           <div className="col-2 p-0 " style={{ maxWidth: "100%", height: "55px" }}>
